1959191ff20e1142e902984fd1b1f5bdbfee4739
MithraKM23 <mithrakm2005@gmail.com>
2026-02-23 13:48:45 +0530

    Coded for game initialization

---
+++ GuessingApp.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class GuessingApp{
+	public static void main(String[] args){
+		System.out.println("Welcome to the guessing App");
+		GameConfig gameconfig=new GameConfig();
+		gameconfig.showRules();
+	}
+}
